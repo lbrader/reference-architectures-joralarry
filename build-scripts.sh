ff96d9cd1cc676d8f61a9464f8d55e2d59ee20cd
snapforbit <snapforbit@gmail.com>
2017-09-10 00:21:56 +0200

    update

--- build-scripts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-imagename="$1"
-export PATH=/opt/conda/bin:$PATH
-source activate vjoaraapp3
-pip install --editable joara-app-provision
-
-joara -d dev image --images $imagename --task build
